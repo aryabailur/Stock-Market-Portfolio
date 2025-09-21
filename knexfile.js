3dff2de0046245b98845eede4d2cbf383b912f32
ahsiri14 <irishaa1404@gmail.com>
2025-09-21 16:44:14 +0530

    Login final backened

---
+++ knexfile.js
@@ -0,0 +1,38 @@
+import 'dotenv/config';
+
+const config = {
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  },
+
+  staging: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  }
+};
+
+export default config;
