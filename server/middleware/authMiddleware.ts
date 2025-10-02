0ef3941bb9a50f59605ac9aa6b6eb38549cce64a
aryabailur <aryabailur@gmail.com>
2025-10-02 13:35:22 +0530

    Restore authMiddleware.ts from stash

---
+++ server/middleware/authMiddleware.ts
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+export async function checkBearer(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const head = req.headers["Authorization"];
+    if (!head) {
+      return res.status(401).json({ message: "Unauthorized error" });
+    }
+    const isBearer = head.startsWith("Bearer");
+    if (!isBearer) {
+      return res.status(401).json({ message: "Unauthorized error" });
+    } else {
+      const authorizationArray = head.split(" ");
+      const token = authorizationArray[1];
+      const secretKey = process.env.JWT_SECRET as string;
+      try {
+        const decodedPayload = jwt.verify(token, secretKey);
+        if (typeof decodedPayload == "object") {
+          req.user = decodedPayload as { id: string; role: string };
+          next();
+        }
+      } catch (Error) {
+        console.log("error in jwt verification", Error);
+        throw Error;
+      }
+    }
+  } catch (Error) {
+    console.log("error", Error);
+    res.status(401).json({ message: "error in middleware" });
+  }
+}
